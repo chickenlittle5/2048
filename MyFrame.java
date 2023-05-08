1cd78e0f1d7380e373381dda41fac7d62ea458d2
Navdeep Singh <56562152+Navdeep051@users.noreply.github.com>
2023-05-08 14:27:45 -0700

    Update MyFrame.java

--- MyFrame.java
+++ MyFrame.java
@@ -7,8 +7,14 @@ public class MyFrame extends JFrame {
 	public MyFrame() {
 		
 		setTitle("2048");
-		getContentPane().setBackground(new java.awt.Color(250,248,239));
+		getContentPane().setBackground(new java.awt.Color(250,248,239,255));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JLabel lbl = new JLabel ( "2048" );
+		
+	     	setLayout( new FlowLayout() );
+	    	add( lbl );
+
 		setSize(600, 800);
 		setVisible(true);
 		
