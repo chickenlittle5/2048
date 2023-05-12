535da9527b947534b03354ef85c597eaa79b4e72
Navdeep Singh <56562152+Navdeep051@users.noreply.github.com>
2023-05-12 14:37:56 -0700

    Update Game.java

--- Game.java
+++ Game.java
@@ -29,6 +29,31 @@ public class Game extends JFrame implements KeyListener {
 		addKeyListener(this);
 		
 	}
+	
+	//Spawns a random block every turn but also checks if there is no block in the spot
+		public void spawn () {
+			int x1 = (int)(Math.random()*4);
+			int y1 = (int)(Math.random()*4);
+			Tile [][] board = grid.getGrid();
+			boolean full = true;
+			for(int r = 0; r < 4; r++) {
+				for (int c = 0; c<4;c++) {
+					if (board[r][c].getValue() == 0) full = false;
+				}
+			}
+			if(!full) {
+				while (board[x1][y1].getValue() != 0) {
+					 x1 = (int)(Math.random()*4);
+					 y1 = (int)(Math.random()*4);
+				}
+				System.out.println(x1 + " " + y1);
+				grid.setGrid(2, x1, y1);
+			}
+			
+			
+			grid.repaint();
+			
+		}
 
 	public void keyPressed(KeyEvent e) {
 		//if (playing) {}
